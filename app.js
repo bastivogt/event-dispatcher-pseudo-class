ba31cce564975600630cef685c27a8571224e8b3
Sevo <betont.online@googlemail.com>
2023-11-20 01:05:39 +0100

    feierabend

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ import Counter from "./sevo/Counter.js";
 
 const counter = new Counter();
 
-console.log(Counter.getEvents().ON_COUNTER_STARTED);
-
 counter.addListener(Counter.getEvents().ON_COUNTER_STARTED, (evt) => {
   console.log(evt.eventType, "count:", evt.target.getCount());
 });
